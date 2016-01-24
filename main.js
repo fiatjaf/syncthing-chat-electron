c3ee6baca8bd4d13162a23e70550db5d77a61c46
fiatjaf <fiatjaf@gmail.com>
2016-01-24 14:48:32 -0200

    electron basics.

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const electron = require('electron')
+const app = electron.app
+
+var window = null
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('ready', () => {
+  window = new electron.BrowserWindow({width: 800, height: 600})
+  window.loadURL(`file://${__dirname}/index.html`)
+  window.webContents.openDevTools()
+  window.on('closed', () => {
+    window = null
+  })
+})
