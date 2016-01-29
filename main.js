e36097a566578e4a7b82be04f9436574c5ffdc85
fiatjaf <fiatjaf@gmail.com>
2016-01-29 17:08:38 -0200

    only open devtools when debugging.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = electron.app
 var window = null
 
 app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
+  if (process.platform !== 'darwin') {
     app.quit()
   }
 })
@@ -14,7 +14,11 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
   window = new electron.BrowserWindow({width: 800, height: 600})
   window.loadURL(`file://${__dirname}/index.html`)
-  window.webContents.openDevTools()
+
+  if (process.env.NODE_DEBUG === 'debug') {
+    window.webContents.openDevTools()
+  }
+
   window.on('closed', () => {
     window = null
   })
